6aa7a3897960454807736dd36d31ea9f9449e3d5
tanziti <tanzitian3@gmail.com>
2023-07-28 15:31:32 -0400

    query1done

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -47,23 +47,27 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the largest casts (i.e., most
   # actors).
+  Movie.joins(:actors)
+  .group('movies.id')
+  .order('COUNT(actors.id) DESC')
+  .limit(3)
+  .select(:id, :title)
+
   
 end
 
 def directed_by_one_of(them)
   # Consider the following:
-  #
   # Movie.where('yr IN (?)', years)
-  #
   # You can use IN to test if an element is present in an array.
-  #
   # Active Record gives you an even better way to write this:
-  #
   # Movie.where(yr: years)
-  #
   # Find the id and title of all the movies directed by one of 'them'.
-  
   # Note: Directors appear in the 'actors' table.
+Movie.select(:id, :title)
+.joins(:director)
+.where(actors: {name: them})
+
 
 end
 
@@ -78,5 +82,5 @@ def movie_names_before_1940
   # improve performance for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+Movie.where('yr < 1940').pluck(:title)
 end
